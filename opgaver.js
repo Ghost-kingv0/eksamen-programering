f1c8e4e87d092e59f73899291a652150a433cebc
mads229u <mads.ebr@gmail.com>
2025-04-04 11:33:36 +0200

    Update opgaver.js

--- opgaver.js
+++ opgaver.js
@@ -1,2 +1,104 @@
 //weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-const opgaveID =  [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+const questions =[{
+    ID: 1,
+    Question:  'images/opgave 1.jpg',
+    answer: [6,7,5,3],
+    correctAnswer: 2
+},
+{
+    ID: 2,
+    Question: 'images/opgave 2.jpg',
+    answer: [20,15,21,25],
+    correctAnswer: 0
+},
+{
+    ID: 3,
+    Question: 'images/opgave 3.jpg',
+    answer: [11,10,13,8],
+    corretAnswer: 1
+},
+{
+    ID: 4,
+    Question: 'images/opgave 4.jpg',
+    answer: [3,5,7,4],
+    corretAnswer: 3
+},
+{
+    ID: 5,
+    Question: 'images/opgave 5.jpg',
+    answer:[14,17,16,15],
+    correctAnswer: 2
+},
+{
+    ID: 6,
+    Question: 'images/opgave 6.jpg',
+    answer:[5,3,6,8],
+    correctAnswer:0
+},
+{
+    ID: 7,
+    Question: 'images/opgave 7.jpg',
+    answer: [4,5,1,3],
+    correctAnswer: 3
+},{
+    ID: 8,
+    Question: 'images/opgave 8.jpg',
+    answer:[4,5,3,6],
+    correctAnswer: 1
+},{
+    ID: 9,
+    Question:'images/opgave 9.jpg',
+    answer:[8,6,5,4],
+    correctAnswer: 0
+
+},{
+    ID: 10,
+    Question: 'images/opgave 10.jpg',
+    answer:[60,40,30,100],
+    correctAnswer: 2
+},{
+    ID: 11,
+    Question: 'images/opgave 11.jpg',
+    answer:[8,7,6,9],
+    correctAnswer: 0
+},{
+    ID: 12,
+    Question: 'images/opgave 12.jpg',
+    answer:[6/10,5/5,1/2,4/10],
+    correctAnswer: 2
+},{
+    ID:13,
+    Question: 'images/opgave 13.jpg',
+    answer:[3/20,3/5,6/20,3/4],
+    correctAnswer: 3
+},{
+    ID:14,
+    Question: 'images/opgave 14.jpg',
+    answer:[0.08,0.16,0.40,0.32],
+    correctAnswer: 0
+},{
+    ID:15,
+    Question: 'images/opgave 15.jpg',
+    answer:[0.25,0.35,0.45,0.55],
+    correctAnswer: 1
+},{
+    ID:16,
+    Question: 'images/opgave 16.jpg',
+    answer: [4,3,5,6],
+    correctAnswer: 1
+},{
+    ID: 17,
+    Question: 'images/opgave 17.jpg',
+    answer:[5,15,20,19],
+    correctAnswer: 3
+},{
+    ID: 18,
+    Question: 'images/opgave 18.jpg',
+    answer: [4,3,1,2],
+    correctAnswer: 1
+},{
+    ID: 19,
+    Question: 'images/opgave 19.jpg',
+    answer:[5,3,2,1],
+    correctAnswer: 3
+}]
