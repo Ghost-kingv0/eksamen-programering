2b32d08383fc13a5b188108eca03a3b3f52deff9
Ghost-kingv0 <Phillipmolesen@gmail.com>
2025-04-07 08:45:44 +0200

    Update QuizCode.js

--- QuizCode.js
+++ QuizCode.js
@@ -1,3 +1,13 @@
-import { questions } from "./opgaver"
+import {questions} from "./opgaver"
+let b = ([questions])
 
-let a = questions
+function shuffleMultipleTimes(array, times) {
+    for (let i = 0; i < times; i++) {
+        array.sort(() => Math.random() - 0.5);
+    }
+    return array;
+}
+
+let a= shuffleMultipleTimes(b,20)
+
+console.log(a)
