7bf3fe1f9e22b057073f85d4a469f2dd2fa2b0ad
mads229u <mads.ebr@gmail.com>
2025-04-08 18:39:57 +0200

    fix

--- løsningsforslag.js
+++ løsningsforslag.js
@@ -15,4 +15,118 @@ løsningsforslag: "brug phytagoros til at regne hypotenusen af trekanten ved at 
     questions: 'images/opgave 3.jpg',
     facit: 10 ,
     løsningsforslag: "brug phytagoros til at regne hypotenusen af trekanten ved at sige a^2+b^2=c^2"
-}]
+},{
+    ID: 4,
+    questions: 'images/opgave 4.jpg',
+    facit: 4 ,
+    løsningsforslag: "brug phytagoros til at regne katetten af trekanten ved at sige c^2-b^2=a^2"
+
+
+},{
+    ID: 5,
+    questions: 'images/opgave 5.jpg',
+    facit: 16 ,
+    løsningsforslag: "brug phytagoros til at regne katetten af trekanten ved at sige c^2-b^2=a^2"
+},{
+    ID: 6,
+    questions: 'images/opgave 6.jpg',
+    facit: 5 ,
+    løsningsforslag: "for at finde hældningen kan man kikke på grafen, og se hvor langt funktionen går op på y-aksen når den går 1 hend på x-aksen"
+},{
+    ID: 7,
+    questions: 'images/opgave 7.jpg',
+    facit: 3 ,
+    løsningsforslag: "for at finde hældningen kan man kikke på grafen, og se hvor langt funktionen går op på y-aksen når den går 1 hend på x-aksen"
+},{
+    ID: 8,
+    questions: 'images/opgave 8.jpg',
+    facit: 5 ,
+    løsningsforslag: "hvis man vil finde b kan man kikke på hvor funktionen skærer y-aksen"
+},{
+    ID: 9,
+    questions: 'images/opgave 9.jpg',
+    facit: 8 ,
+    løsningsforslag: "for at finde arealet på en retvinklet trekant kan du bruge formlen 1/2*G*h"
+},{
+    ID: 10,
+    questions: 'images/opgave 10.jpg',
+    facit: 5 ,
+    løsningsforslag: "for at finde arealet på en retvinklet trekant kan du bruge formlen 1/2*G*h"
+},{
+    ID: 11,
+    questions: 'images/opgave 11.jpg',
+    facit: 8 ,
+    løsningsforslag: "for at finde gennemsnittet kan vi finde summen og dividere det med antallet af værdier"
+},{
+    ID: 12,
+    questions: 'images/opgave 12.jpg',
+    facit: 1/2 ,
+    løsningsforslag: "for at finde sandsynlighede af at trække en rød kugle kan vi tage antallet af røde kugler og så dividere det med antallet af kugler i alt"
+},{
+    ID: 13,
+    questions: 'images/opgave 13.jpg',
+    facit: 3/4 ,
+    løsningsforslag: "    for at finde sandsynlighede af at trække en blå eller grøn kugle kan vi tage antallet af blå og grønne kugler og så dividere det med antallet af kugler i alt"
+
+},{
+    ID: 14,
+    questions: 'images/opgave 14.jpg',
+    facit: 0.04 ,
+    løsningsforslag: "for at finde resultatet kan vi ligge chancen for at se en sort bil sammen siden det skal ske to gange i træke, hvilekt vi gøre ved at sige 0,20*0,20=0,04"
+},{
+    ID: 15,
+    questions: 'images/opgave 15.jpg',
+    facit: 35 ,
+    løsningsforslag: "for at få svaret til denne kan vi ligge chancen for at få de to klovne sammen og så minus det med 100 for at få resultatet, hved at sige 75%-100%=35%"
+},{
+    ID: 16,
+    questions: 'images/opgave 16.jpg',
+    facit: 3 ,
+    løsningsforslag: "kik på antallet af løbere der har løbet over 45 minutter"
+},{
+    ID: 17,
+    questions: 'images/opgave 17.jpg',
+    facit: 19 ,
+    løsningsforslag: "kikker på antallet af elever på grafen der ligger over 4 timer og finder summen"
+},{
+    ID: 18,
+    questions: 'images/opgave 18.jpg',
+    facit: 1 ,
+    løsningsforslag: "kik på antallet af dykkere der holder været under vandet i 50 sekunder"
+},{
+    ID: 19,
+    questions: 'images/opgave 19.jpg',
+    facit: 5 ,
+    løsningsforslag: "du kan her finde x ved at sige 3x+5=20, 3x+5-5=20-5, 3x=15, 3x/3=15/3, x=5"
+},{
+    ID: 20,
+    questions: 'images/opgave 20.jpg',
+    facit: 4 ,
+    løsningsforslag: "du kan her finde x ved at sige 2(x+3)=14, 2x+6=14, 2x+6-6=14-6, 2x=8, 2x/2=8/2, x=4"
+},{
+    ID: 21,
+    questions: 'images/opgave 21.jpg',
+    facit: 5 ,
+    løsningsforslag: " du kan her finde x ved at sige 5x-4=21, 5x-4+4=21+4, 5x=25, 5x/5=25/5, x=5"
+},{
+    ID: 22,
+    questions: 'images/opgave 22.jpg',
+    facit: 4 ,
+    løsningsforslag: " du kan her finde x ved at sige 2(x+4)=16 , 2x+8=16, 2x+8-8=16-8, 2x=8, 2x/2 = 8/2, x=4"
+},{
+    ID: 23,
+    questions: 'images/opgave 23.jpg',
+    facit: 3.5,
+    løsningsforslag: " du kan her finde x ved at sige 5(2x-3)=20, 10x-15=20, 10x-15+15=20+15, 10x=35, 10x/10=35/10, x=3.5 "
+},{
+    ID: 24,
+    questions: 'images/opgave 24.jpg',
+    facit: 4.6 ,
+    løsningsforslag: " du kan her finde x ved at sige 3(x-1)+2(x+5)=25, 3x-3+2x+5=25, 5x+2=25, 5x+2-2=25-2, 5x=23, 5x/5=23/5, x=4.6"
+},{
+    ID: 25,
+    questions: 'images/opgave 21.jpg',
+    facit: 5 ,
+    løsningsforslag: " du kan her finde x ved at sige 0.5x+2.3=4.8, 0.5x+2.3-2.3=4.8-2.3, 0.5x=2.5, 2*0.5x=2.5*2, x=5"
+}, 
+]
