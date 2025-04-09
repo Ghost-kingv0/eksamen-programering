ccc0f75bbdde5453b82a7cfa9ce4d8326ea02d37
Ghost-kingv0 <Phillipmolesen@gmail.com>
2025-04-09 12:57:19 +0200

    Update QuizCode.js

--- QuizCode.js
+++ QuizCode.js
@@ -1,22 +1,15 @@
 const { questions } = require('./opgaver.js');
 
-// Create a shallow copy of the questions array
-let b = [...questions];
 
-function shuffleMultipleTimes(array, times) {
-    let shuffledArray = [...array]; // Create a copy to avoid modifying the original array
+
+function shuffleMultipleTimes(times) {
+    let shuffledArray = [...questions]; // Create a copy to avoid modifying the original array
     for (let i = 0; i < times; i++) {
         shuffledArray.sort(() => Math.random() - 0.5);
     }
     return shuffledArray;
 }
 
-// Shuffle the copied array 20 times
-let a = shuffleMultipleTimes(b, 20);
-
-
-console.log(a);
-console.log(a[0].answer[0]);
-
-
+ Medule.exports = { shuffleMultipleTimes };
+ 
 function keepingScore(){}
